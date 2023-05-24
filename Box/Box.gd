e2e5243aab4dc960a02de207b9d700e985646f1c
Mikael Hermansson <mikael@hermansson.io>
2023-05-24 17:32:37 +0200

    Refactored Box and DestroyedBox
    - Changed Box to use collision layers instead of disabling its shape
    - Changed Box to freeze upon destruction
    - Changed Box to hide upon destruction
    - Changed root node in DestroyedBox from RigidBody3D to Node3D
    - Renamed the pieces in DestroyedBox
    - Changed pieces in DestroyedBox to be hidden in `_ready_` instead
    - Changed pieces in DestroyedBox to not be sleeping at the start

--- Box/Box.gd
+++ Box/Box.gd
@@ -9,6 +9,11 @@ const DESTROYED_BOX_SCENE := preload("res://Box/DestroyedBox.tscn")
 
 
 func damage(_impact_point: Vector3, _force: Vector3):
+	freeze = true
+	visible = false
+	collision_layer = 0
+	collision_mask = 0
+
 	for i in range(COINS_COUNT):
 		var coin := COIN_SCENE.instantiate()
 		get_parent().add_child(coin)
@@ -19,7 +24,6 @@ func damage(_impact_point: Vector3, _force: Vector3):
 	get_parent().add_child(destroyed_box)
 	destroyed_box.global_position = global_position
 	
-	_collision_shape.set_deferred("disabled", true)
 	_destroy_sound.pitch_scale = randfn(1.0, 0.1)
 	_destroy_sound.play()
 	await _destroy_sound.finished
--- Box/DestroyedBox.gd
+++ Box/DestroyedBox.gd
@@ -1,22 +1,28 @@
-extends RigidBody3D
+extends Node3D
 
 const FLYING_PIECES := 3
 const THROW_STRENGTH := 500
 
 @onready var _pieces_idx := [0, 1, 2, 3, 4, 5]
 
-
 func _ready() -> void:
+	$Piece1.hide()
+	$Piece2.hide()
+	$Piece3.hide()
+	$Piece4.hide()
+	$Piece5.hide()
+	$Piece6.hide()
+
 	_pieces_idx.shuffle()
 
 	for i in range(FLYING_PIECES):
 		var piece_idx: int = _pieces_idx[i]
 		var piece: RigidBody3D = get_child(piece_idx)
-		piece.show()
+		piece.collision_layer = 0b10
+		piece.collision_mask = 0b10
 		piece.freeze = false
-		piece.sleeping = false
-		piece.set_collision_mask_value(1, true)
-		
+		piece.visible = true
+
 		var rand_vector := (Vector3.ONE * 0.5) - Vector3(randf(), randf(), randf())
-		
+
 		piece.apply_force(rand_vector * THROW_STRENGTH, rand_vector)
